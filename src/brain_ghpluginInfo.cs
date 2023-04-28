eb4ccd2722bcbbe578a4ccb72cf4ede14a8ec25c
garciadelcastillo <personal@garciadelcastillo.es>
2023-04-28 10:39:20 -0400

    Initial VS project

---
+++ src/brain_ghpluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace brain_ghplugin
+{
+    public class brain_ghpluginInfo : GH_AssemblyInfo
+    {
+        public override string Name => "brain_ghplugin";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("AC0D367B-B469-4920-A3CB-3DE02C5D2170");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
