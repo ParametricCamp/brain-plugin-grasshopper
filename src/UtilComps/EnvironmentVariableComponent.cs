ffcafab080d0a6007c9be04d63ef93fdcf0d3ff2
garciadelcastillo <personal@garciadelcastillo.es>
2023-05-19 11:40:09 -0400

    Added Environment Variable component

---
+++ src/UtilComps/EnvironmentVariableComponent.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Brain.UtilComps
+{
+    public class EnvironmentVariableComponent : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the EnvironmentVariableComponent class.
+        /// </summary>
+        public EnvironmentVariableComponent()
+          : base("Environment Variable", "Env Var",
+              "Get the value of a system environment variable",
+              "Brain", "Utils")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Name", "N", "Name of the variable to fetch", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Value", "V", "Value of the environment variable", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            string name = "";
+            if (!DA.GetData(0, ref name)) return;
+
+            // Safe-proof the env var fetch
+            try
+            {
+                var val = Environment.GetEnvironmentVariable(name);
+                DA.SetData(0, val);
+            }
+            catch (Exception ex)
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Cannot retrieve environment variable " + name + ": " + ex.Message);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("9B9F7305-2296-4344-8545-C9CC27D865F0"); }
+        }
+    }
+}
