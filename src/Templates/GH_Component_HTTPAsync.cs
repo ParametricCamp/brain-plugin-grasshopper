1656a972e66a73ea2ab4b36081f681622efe5a90
Richard Aubin <richard.aubin@hotmail.com>
2023-05-27 21:15:20 +0800

    Changed BrainHttp to static class.  Updated GH_Component_HTTPSync to use same simplified format as Async.

--- src/Templates/GH_Component_HTTPAsync.cs
+++ src/Templates/GH_Component_HTTPAsync.cs
@@ -8,16 +8,55 @@ namespace Brain.Templates
 {
     public abstract class GH_Component_HTTPAsync : GH_Component
     {
-        private readonly BrainHttp _http;
-
         protected string _response = "";
         protected bool _shouldExpire = false;
         protected RequestState _currentState = RequestState.Off;
 
         public GH_Component_HTTPAsync(string name, string nickname, string description, string category, string subcategory)
-    : base(name, nickname, description, category, subcategory)
+            : base(name, nickname, description, category, subcategory)
         {
-            _http = new BrainHttp();
+        }
+
+        protected void GETAsync(
+            string url,
+            string authorization,
+            int timeout)
+        {
+            GetResponse(() => BrainHttp.GetResponseFromGet(url, authorization, timeout));
+        }
+
+        protected void POSTAsync(
+            string url,
+            string body,
+            string contentType,
+            string authorization,
+            int timeout)
+        {
+            GetResponse(() => BrainHttp.GetResponseFromPost(body, url, contentType, authorization, timeout));
+        }
+
+        private void GetResponse(Func<string> getAsyncWebResponse)
+        {
+            Task.Run(() =>
+            {
+                var response = "";
+
+                try
+                {
+                    response =  getAsyncWebResponse.Invoke();
+                    _currentState = RequestState.Done;
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message;
+                    _currentState = RequestState.Error;
+                }
+                finally
+                {
+                    _shouldExpire = true;
+                    RhinoApp.InvokeOnUiThread((Action)delegate { ExpireSolution(true); });
+                }
+            });
         }
 
         protected override void ExpireDownStreamObjects()
@@ -28,46 +67,5 @@ namespace Brain.Templates
             }
         }
 
-        protected void GETAsync(
-            string url,
-            string authorization,
-            int timeout)
-        {
-            GetWebResponse(() => _http
-            .GetResponseFromGet(url, authorization, timeout));
-        }
-
-        protected void POSTAsync(
-            string url,
-            string body,
-            string contentType,
-            string authorization,
-            int timeout)
-        {
-            GetWebResponse(() => _http
-                .GetResponseFromPost(body, url, contentType, authorization, timeout));
-        }
-
-        private void GetWebResponse(Func<string> getAsyncWebResponse)
-        {
-            Task.Run(() =>
-            {
-                try
-                {
-                    _response = getAsyncWebResponse.Invoke();
-                    _currentState = RequestState.Done;
-                }
-                catch (Exception ex)
-                {
-                    _response = ex.Message;
-                    _currentState = RequestState.Error;
-                }
-                finally
-                {
-                    _shouldExpire = true;
-                    RhinoApp.InvokeOnUiThread((Action)delegate { ExpireSolution(true); });
-                }
-            });
-        }
     }
 }
